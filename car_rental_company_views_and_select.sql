27a904b59b34089aef73a0316bc81ab318fe592b
Erazm Malarczyk <malarczykerazm@gmail.com>
2017-08-10 14:57:57 +0200

    First 10 selects added.

--- car_rental_company_views_and_select.sql
+++ car_rental_company_views_and_select.sql
@@ -5,14 +5,12 @@ SET sql_mode = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_AUTO_CREATE_USER,NO_EN
 use car_rental_company;
 
 /* a) Stwórz widok który przedstawi klientów wraz z datami wypożyczeń i markami samochodów. */
-CREATE VIEW customers_with_rental_data AS
+CREATE VIEW customers_with_all_rentals_data AS
 SELECT cu.ID, cu.firstName, cu.surname, r.dateSince, r.dateTo, ca.brand 
 FROM customers cu
 JOIN rental_data r ON cu.ID = r.customerID
 JOIN cars ca ON ca.ID = r.carID
-ORDER BY cu.ID;
-
-select * from customers_with_rental_data;
+ORDER BY cu.ID, ca.brand;
 
 /* b) Stwórz dodatkowe 2 widoki pozwalające łatwiej wykonać jakieś czynności z zadania 4. */
 CREATE VIEW cars_between_200000_and_300000_mileage AS
@@ -21,19 +19,16 @@ FROM cars c
 WHERE mileage >= 200000 and mileage <= 300000
 ORDER BY mileage DESC;
 
-CREATE VIEW customers_with_rental_number AS
+CREATE VIEW customers_with_all_rentals_number AS
 SELECT c.ID, c.firstName, c.surname, count(r.ID) 
 FROM customers c
 JOIN rental_data r ON c.ID = r.customerID
 GROUP BY c.ID
-ORDER BY count(r.ID) DESC;
+ORDER BY count(r.ID) DESC, ID;
 
 /** Zadanie 4 **/
 
 /**
-h) znajdź klientów, którzy wypożyczyli największą liczbę różnych samochodów
-i) znajdź samochody najczęściej wypożyczane
-j) znajdź klientów, którzy zapłacili najwięcej za wypożyczenia w tym roku
 k) znajdź klientów, którzy wypożyczyli samochód X w dacie miedzy Y a Z.
 l) zaktualizuj kolor samochodu X
 m) znajdź klienta, który najczęściej wypożycza i oddaje samochody w innych miejscach
@@ -61,4 +56,29 @@ select count(*) from cars_between_200000_and_300000_mileage;
 select agencyID, firstName, surname, positionID from employees where positionID = 3 order by agencyID;
 
 /* g) znajdź klientów z największa liczbą wypożyczeń - przyjęto, że chodzi o 5 klientów */
-select * from customers_with_rental_number limit 5;
+select * from customers_with_all_rentals_number limit 5;
+
+/* h) znajdź klientów, którzy wypożyczyli największą liczbę różnych samochodów - interpretuję różne samochody jako różne marki samochodów
+dla większego zróżnicowania uzyskanych wyników, przyjęto, że chodzi o 5 klientów */
+select ID, firstName, surname, count(distinct brand)
+from customers_with_all_rentals_data
+group by ID
+order by count(distinct brand) desc, ID
+limit 5;
+
+/* i) znajdź samochody najczęściej wypożyczane - przyjęto, że chodzi o 3 samochody rozróżniane po ich ID */
+select c.ID, c.brand, c.model, c.productionYear, c.engineVolume, c.mileage, count(r.ID)
+from cars c
+join rental_data r on c.ID = r.carID
+group by c.ID
+order by count(r.ID) desc, c.ID
+limit 3;
+
+
+/* j) znajdź klientów, którzy zapłacili najwięcej za wypożyczenia w tym roku - przyjęto, że chodzi o 5 klientów */
+select r.customerID, c.firstName, c.surname, sum(r.price)
+from customers c join rental_data r on c.ID = r.customerID
+where year(r.dateSince) = year(curdate())
+group by r.customerID
+order by sum(r.price) desc
+limit 5;
